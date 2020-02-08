6c2b4564ed48cb427c5a0c5666ad44e326ce74e4
pervinpashazade <ParvinKHp@code.local>
2020-02-08 18:59:48 +0400

    AddLanguageForm Added. UI 100% Completed. Validations OK.

---
+++ LibraryFinalTask/Forms/AddPositionForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryFinalTask.Forms
+{
+    public partial class AddPositionForm : Form
+    {
+        public AddPositionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                lblErrorName.Show();
+            }
+            else
+            {
+                lblErrorName.Hide();
+            }
+
+            if (!(rBtnStatusActive.Checked) && !(rBtnStatusDisabled.Checked))
+            {
+                lblErrorStatus.Show();
+            }
+            else
+            {
+                lblErrorStatus.Hide();
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtSearch.Clear();
+
+            lblErrorName.Hide();
+            lblErrorStatus.Hide();
+        }
+
+        private void IconBackspace_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+        }
+
+        private void IconSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSearch.Text))
+            {
+                if (string.IsNullOrEmpty(txtSearch.Text))
+                {
+                    MessageBox.Show("Input can't be empty for search somethings!", "Oops, Error!");
+                }
+            }
+        }
+    }
+}
