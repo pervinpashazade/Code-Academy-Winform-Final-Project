fdf8d6da0b323d94a32a7bd0d92ebdcfc391b305
pervinpashazade <ParvinKHp@code.local>
2020-02-16 13:33:58 +0400

    ReportsForm load orders OK

--- LibraryFinalTask/Forms/ReportsForm.Designer.cs
+++ LibraryFinalTask/Forms/ReportsForm.Designer.cs
@@ -28,22 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportsForm));
             this.dateStart = new System.Windows.Forms.DateTimePicker();
             this.dateEnd = new System.Windows.Forms.DateTimePicker();
             this.dgvReports = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.iconRefresh = new System.Windows.Forms.PictureBox();
+            this.btnExport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReports)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconRefresh)).BeginInit();
             this.SuspendLayout();
             // 
             // dateStart
             // 
+            this.dateStart.Cursor = System.Windows.Forms.Cursors.Hand;
             this.dateStart.Location = new System.Drawing.Point(54, 45);
             this.dateStart.Name = "dateStart";
             this.dateStart.Size = new System.Drawing.Size(200, 20);
@@ -51,7 +56,8 @@
             // 
             // dateEnd
             // 
-            this.dateEnd.Location = new System.Drawing.Point(541, 45);
+            this.dateEnd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.dateEnd.Location = new System.Drawing.Point(313, 45);
             this.dateEnd.Name = "dateEnd";
             this.dateEnd.Size = new System.Drawing.Size(200, 20);
             this.dateEnd.TabIndex = 1;
@@ -65,13 +71,12 @@
             this.dgvReports.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
             this.Column2,
-            this.Column3,
             this.Column4,
             this.Column7,
             this.Column8,
             this.Column5,
             this.Column6});
-            this.dgvReports.Location = new System.Drawing.Point(12, 149);
+            this.dgvReports.Location = new System.Drawing.Point(12, 113);
             this.dgvReports.Name = "dgvReports";
             this.dgvReports.ReadOnly = true;
             this.dgvReports.Size = new System.Drawing.Size(776, 289);
@@ -90,12 +95,6 @@
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
             this.Column2.Visible = false;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Customer";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
             // 
             // Column4
             // 
@@ -127,17 +126,55 @@
             this.Column6.Name = "Column6";
             this.Column6.ReadOnly = true;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.Location = new System.Drawing.Point(563, 42);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 25);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // iconRefresh
+            // 
+            this.iconRefresh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconRefresh.Image = ((System.Drawing.Image)(resources.GetObject("iconRefresh.Image")));
+            this.iconRefresh.Location = new System.Drawing.Point(749, 42);
+            this.iconRefresh.Name = "iconRefresh";
+            this.iconRefresh.Size = new System.Drawing.Size(39, 25);
+            this.iconRefresh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconRefresh.TabIndex = 47;
+            this.iconRefresh.TabStop = false;
+            this.iconRefresh.Click += new System.EventHandler(this.IconRefresh_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(698, 408);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(90, 32);
+            this.btnExport.TabIndex = 48;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.BtnExport_Click);
+            // 
             // ReportsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.iconRefresh);
+            this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.dgvReports);
             this.Controls.Add(this.dateEnd);
             this.Controls.Add(this.dateStart);
             this.Name = "ReportsForm";
-            this.Text = "ReportsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Library App - Reports";
             ((System.ComponentModel.ISupportInitialize)(this.dgvReports)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconRefresh)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -147,13 +184,15 @@
         private System.Windows.Forms.DateTimePicker dateStart;
         private System.Windows.Forms.DateTimePicker dateEnd;
         private System.Windows.Forms.DataGridView dgvReports;
+        private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.PictureBox iconRefresh;
+        private System.Windows.Forms.Button btnExport;
     }
 }
--- LibraryFinalTask/Forms/ReportsForm.cs
+++ LibraryFinalTask/Forms/ReportsForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryFinalTask.Data;
+using LibraryFinalTask.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +14,55 @@ namespace LibraryFinalTask.Forms
 {
     public partial class ReportsForm : Form
     {
+        private readonly LibraryDbContext _db;
         public ReportsForm()
         {
+            _db = new LibraryDbContext();
+
             InitializeComponent();
         }
 
+        private void BtnSearch_Click(object sender, EventArgs e)
+        {
+            if (dateEnd.Value > dateStart.Value)
+            {
+                DialogResult d = MessageBox.Show("The end date cannot be smaller than the start date", "Oops, Search Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                return;
+            }
+
+            dgvReports.Rows.Clear();
+
+            var query = _db.OrderItems.Include("Order")
+                                       .Include("Book")
+                                       .Where(o => (o.OrderDate.Year == dateStart.Value.Year &&
+                                        o.OrderDate.Month == dateStart.Value.Month &&
+                                        o.OrderDate.Day == dateStart.Value.Day)
+                                        &&
+                                        (o.OrderDate.Year <= dateEnd.Value.Year &&
+                                        o.OrderDate.Month <= dateEnd.Value.Month &&
+                                        o.OrderDate.Day <= dateEnd.Value.Day))
+                                        .ToList();
+
+            List<OrderItem> orders = query;
+
+            foreach (var item in orders)
+            {
+                dgvReports.Rows.Add(item.Id, item.OrderId, item.Book.Name, item.Count,
+                                    item.Amount, item.OrderDate, item.ReturnDate);
+            }
+
+        }
+
+        private void IconRefresh_Click(object sender, EventArgs e)
+        {
+            dateStart.Value = DateTime.Now;
+            dateEnd.Value = DateTime.Now;
+            dgvReports.Rows.Clear();
+        }
+
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
